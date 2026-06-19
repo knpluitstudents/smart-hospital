368bdc544f306dbd5379162f7464ebe14117e3c3
knpluitstudents <kodingnextpluitstudents@gmail.com>
2026-06-19 13:47:55 +0700

    update

--- hospital_app.py
+++ hospital_app.py
@@ -1,1 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as numpy 
+import pickle
+import os
 
+st.set_page_config(
+    page_title="Smart Hospital Patient Navigator",
+    page_icon="",
+    layout="wide"
+)
